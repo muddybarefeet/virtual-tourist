fb965512458be6f201c94fd0485bb4dc3bed02f7
Anna Rogers <2muddybarefeet@gmail.com>
2016-07-25 07:39:33 -0700

    initial commit

---
+++ virtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoAlbumViewController.swift
+//  virtualTourist
+//
+//  Created by Anna Rogers on 7/25/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+class PhotoAlbumViewController {
+    
+    //deal with data persistence
+    
+}
---
+++ virtualTourist/TravelLocationsMapView.swift
@@ -0,0 +1,17 @@
+//
+//  TravelLocationsMapView.swift
+//  virtualTourist
+//
+//  Created by Anna Rogers on 7/25/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+import MapKit
+
+class TravelLocationsMapView {
+    
+    //deal with user map interation -> modally present next controller on tap of a pin
+    
+}
