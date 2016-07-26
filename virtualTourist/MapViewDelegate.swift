457f491272599c466a008dc006de434561ef99fa
Anna Rogers <2muddybarefeet@gmail.com>
2016-07-25 19:32:51 -0700

    add pin to map on long click

---
+++ virtualTourist/MapViewDelegate.swift
@@ -0,0 +1,57 @@
+//
+//  MapViewDelegate.swift
+//  virtualTourist
+//
+//  Created by Anna Rogers on 7/25/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewDelegate: NSObject, MKMapViewDelegate {
+    
+    //on click and hold = add a pin
+    
+    //on click a pin geocode the address and show modal of the images from that location mapView:didSelectAnnotationView:
+    
+//    // method to place the pin on the map and how it is styled
+//    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+//        let reuseId = "pin"
+//        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+//        if pinView == nil {
+//            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+//            //tell the pin if extra information can be show about the pin boolean
+//            pinView!.canShowCallout = true
+//            pinView!.pinTintColor = UIColor.redColor()
+//            //view to display on the right side of the standard callout bubble
+//            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+//        }
+//        else {
+//            pinView!.annotation = annotation
+//        }
+//        return pinView
+//    }
+    
+    
+//    //on click of a pin, open the url in the subtitle
+//    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+//        if control == view.rightCalloutAccessoryView {
+//            let app = UIApplication.sharedApplication()
+//            if var mediaURL = (view.annotation?.subtitle!)! as String? {
+//                if (mediaURL.hasPrefix("www")) {
+//                    //add https:// to the front if they do not have this
+//                    mediaURL = "https://" + mediaURL
+//                }
+//                let nsURL = NSURL(string: mediaURL)!
+//                let isOpenable = app.canOpenURL(nsURL)
+//                if isOpenable {
+//                    app.openURL(nsURL)
+//                } else {
+//                    print("error in the URL")
+//                }
+//            }
+//        }
+//    }
+    
+}
--- virtualTourist/TravelLocationsMapView.swift
+++ virtualTourist/TravelLocationsMapView.swift
@@ -10,8 +10,24 @@
 import UIKit
 import MapKit
 
-class TravelLocationsMapView {
+class TravelLocationsMapView: UIViewController {
     
+    @IBOutlet weak var mapView: MKMapView!
     //deal with user map interation -> modally present next controller on tap of a pin
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let longUIPress = UILongPressGestureRecognizer(target: self, action: #selector(self.action))
+        longUIPress.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(longUIPress)
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer){
+        print("action to add annotation called")
+        let touchPoint = gestureRecognizer.locationInView(mapView)
+        let newCoordinates = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinates
+        mapView.addAnnotation(annotation)
+    }
     
 }
